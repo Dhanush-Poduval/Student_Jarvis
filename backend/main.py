8517dd83efbaad050a1074c7f0dfffc48f472f83
Dhanush-Poduval <dhanushpoduval@gmail.com>
2025-08-26 02:07:03 +0530

    feat:made the text searching in chunks for large pdfs and better accuracy

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI,UploadFile,File,Form
 import PyPDF2
 import io
-import torch
 from transformers import pipeline
 
 app =FastAPI()
@@ -9,18 +8,37 @@ app =FastAPI()
 qa_pipeline=pipeline("question-answering",model="deepset/roberta-large-squad2")
 
 text=""
+def chunk_text(text,chunk_size=400,overlaps=50):
+    words=text.split()
+    chunks=[]
+    start=0
+    while start<len(words):
+        end=min(start+chunk_size,len(words))
+        chunk=" ".join(words[start:end])
+        chunks.append(chunk)
+        start+=chunk_size-overlaps
+    return chunks
 def ask_agent(question , context_text):
-    result=qa_pipeline(question=question,context=context_text)
-    print(f"Confidence score: {result['score']:.2f}")
+   chunks=chunk_text(context_text)
+   best_score=0
+   best_answer="No answer found"
+   for chunk in chunks:
+    result = qa_pipeline(question=question, context=chunk)
+    if result['score']>best_score:
+        best_score=result['score']
+        best_answer=result['answer']
+   print(f"the best score is :{best_score:.2f}")
+   return best_answer
+
    
     
-    return result['answer']
+    
 
 def extract_pdf(file:io.BytesIO):
     pdf_reader=PyPDF2.PdfReader(file)
     text=""
     for pages in pdf_reader.pages:
-        text+=pages.extract_text()or""
+        text+=pages.extract_text()or"Empty file"
     return text
 
 
