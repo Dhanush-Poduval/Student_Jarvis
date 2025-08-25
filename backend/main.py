a0fb3d04b8be55531495fa2d570080eadb834177
Dhanush-Poduval <dhanushpoduval@gmail.com>
2025-08-25 23:25:48 +0530

    feat:made the pdf reading endpoint

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,UploadFile,File
+import PyPDF2
+import io
 
 app =FastAPI()
 
-@app.get('/test')
-def test():
-    return "Successfull mate"
+def extract_pdf(file:io.BytesIO):
+    pdf_reader=PyPDF2.PdfReader(file)
+    text=""
+    for pages in pdf_reader.pages:
+        text+=pages.extract_text()or""
+    return text
+
+
+@app.post('/student_pdf')
+async def upload_pdf(file:UploadFile=File(...)):
+    file_content=await file.read()
+    pdf_file=io.BytesIO(file_content)
+    global text
+    text=extract_pdf(pdf_file)
+    return {'The text is ':text[:500]}
+
