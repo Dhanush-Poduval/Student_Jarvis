545ba5f49e4c96a11354e94fab3a9a6cd6f55600
Dhanush-Poduval <dhanushpoduval@gmail.com>
2025-08-29 17:54:18 +0530

    feat:made the connection of database and added values

--- backend/models.py
+++ backend/models.py
@@ -6,4 +6,4 @@ class User(Base):
     id=Column(Integer,primary_key=True,index=True)
     name=Column(String)
     email=Column(String , unique=True)
-    password_hasg=Column(String,nullable=False)
+    password=Column(String,nullable=False)
--- backend/routers/user.py
+++ backend/routers/user.py
@@ -1,4 +1,8 @@
-from fastapi import FastAPI,APIRouter
+from fastapi import FastAPI,APIRouter,Depends
+from .. import models,schemas,database
+from sqlalchemy.orm import Session
+from ..database import get_db
+from typing import List
 
 app=FastAPI()
 
@@ -6,6 +10,10 @@ router=APIRouter(
     tags=['User']
 )
 
-@router.get('/test')
-def test():
-    return{'works successfully'}
+@router.post('/user')
+def create_user(signup:schemas.Signup , db:Session=Depends(database.get_db)):
+    new_user=models.User(name=signup.name , email=signup.email , password=signup.password)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
