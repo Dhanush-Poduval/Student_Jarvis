10180f5614f18281248cc40505f5b72ab0701117
Dhanush-Poduval <dhanushpoduval@gmail.com>
2025-08-30 17:57:57 +0530

    feat:made the format for the frontend

---
+++ frontend/app/dashboard/page.js
@@ -0,0 +1,9 @@
+'use client'
+import Dashboard from '@/LandingPage/components/Dashboard'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div><Dashboard /></div>
+  )
+}
--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,7 +1,18 @@
+'use client'
+import Benefits from '@/LandingPage/Benefits'
+import Features from '@/LandingPage/Features'
+import Hero from '@/LandingPage/Hero'
+import Navbar from '@/LandingPage/Navbar'
 import React from 'react'
 
 export default function page() {
   return (
-    <div className='bg-red-500'>page</div>
+    <div>
+      <Navbar/>
+      <Hero />
+      <Features />
+      <Benefits />
+    </div>
+    
   )
 }
