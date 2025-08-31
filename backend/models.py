d97c2d9858fa903e2d784c388cd7eb28b0101a34
Dhanush-Poduval <dhanushpoduval@gmail.com>
2025-08-31 12:03:28 +0530

    feat:made the file storage database

--- backend/models.py
+++ backend/models.py
@@ -1,5 +1,9 @@
 from .database import Base
-from sqlalchemy import Column,Integer,String
+from sqlalchemy import Column,Integer,String,ForeignKey,DateTime
+from datetime import datetime, timezone, timedelta
+
+IST=timezone(timedelta(hours=5,minutes=30))
+
 
 class User(Base):
     __tablename__="user"
@@ -7,3 +11,12 @@ class User(Base):
     name=Column(String)
     email=Column(String , unique=True)
     password=Column(String,nullable=False)
+
+
+class Documents(Base):
+    __tablename__="documents"
+    id=Column(Integer,primary_key=True,index=True)
+    user_id=Column(Integer,ForeignKey("user.id"))
+    filename=Column(String)
+    file_path=Column(String)
+    uploaded_at=Column(DateTime(timezone=True),default=lambda:datetime.now(IST))
