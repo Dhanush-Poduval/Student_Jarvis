431db833d865e3d32c7aa00f37fc9cef26255227
Dhanush-Poduval <dhanushpoduval@gmail.com>
2025-09-02 18:45:16 +0530

    feat:Deleted the frotnend folder to redo

--- frontend/app/dashboard/page.js
+++
@@ -1,9 +0,0 @@
-'use client'
-import Dashboard from '@/LandingPage/components/Dashboard'
-import React from 'react'
-
-export default function page() {
-  return (
-    <div><Dashboard /></div>
-  )
-}
