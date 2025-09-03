880e044880cc6259624af0a3f1efce95fc5602c1
Dhanush-Poduval <dhanushpoduval@gmail.com>
2025-09-03 23:19:41 +0530

    feat:added cors for interaction with frontend

--- backend/main.py
+++ backend/main.py
@@ -2,8 +2,23 @@ from fastapi import FastAPI
 from . import models
 from.database import engine
 from .routers import user , aimodel
-
+from fastapi.middleware.cors import CORSMiddleware
 app=FastAPI()
+
+origins = [
+    "http://localhost:3000",  # Next.js local dev
+    "http://127.0.0.1:3000", # just in case
+    # you can add deployed frontend URLs here too
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,          # Allowed origins
+    allow_credentials=True,
+    allow_methods=["*"],            # GET, POST, PUT, DELETE etc.
+    allow_headers=["*"],            # Allow all headers
+)
+
 
 models.Base.metadata.create_all(bind=engine)
 
