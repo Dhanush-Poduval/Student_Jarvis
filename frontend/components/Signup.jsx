594f8788cba81fd83082a06cbfe78884f2cd6959
Dhanush-Poduval <dhanushpoduval@gmail.com>
2025-09-04 00:00:58 +0530

    feat:made rhe signup page post logic

--- frontend/components/Signup.jsx
+++ frontend/components/Signup.jsx
@@ -18,6 +18,43 @@ import { useRouter } from "next/navigation"
 import { useState } from "react"
 
 export default function Signup() {
+    const [name,setName]=useState("")
+    const [email,setEmail]=useState("")
+    const [password,setPassword]=useState("")
+    const router=useRouter()
+    const handleName=(e)=>{
+      const text=e.target.value
+      setName(text)
+      console.log(text)
+    }
+    const handleEmail=(e)=>{
+        const text=e.target.value
+        setEmail(text)
+        console.log(text)
+    }
+    const handlePassword=(e)=>{
+        const text=e.target.value
+        setPassword(text)
+        console.log(text)
+    }
+    const handleSubmit=async(e)=>{
+      e.preventDefault()
+      try{
+        const res=await fetch('http://127.0.0.1:8000/user',{
+            method:'POST',
+            headers:{'Content-Type':'application/json'},
+            body:JSON.stringify({
+                "name":name,
+                "email":email,
+                "password":password
+            })})
+        const data=await res.json()
+        console.log(data)
+        router.push('/')
+      }catch(error){
+        console.log("error",error)
+      }
+    }
   return (
     <div className='flex items-center justify-center mt-32'>
         <Card className="w-full max-w-sm">
@@ -34,14 +71,15 @@ export default function Signup() {
         </CardAction>
       </CardHeader>
       <CardContent>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="flex flex-col gap-6">
             <div className="grid gap-2">
-              <Label htmlFor="email">Email</Label>
+              <Label htmlFor="name">Name</Label>
               <Input
                 id="name"
-                placeholder="m@example.com"
+                placeholder="Enter your name"
                 required
+                onChange={handleName}
                 
               />
             </div>
@@ -52,6 +90,7 @@ export default function Signup() {
                 type="email"
                 placeholder="m@example.com"
                 required
+                onChange={handleEmail}
                 
               />
             </div>
@@ -59,7 +98,7 @@ export default function Signup() {
               <div className="flex items-center">
                 <Label htmlFor="password">Password</Label>
               </div>
-              <Input id="password" type="password" required />
+              <Input id="password" type="password" required onChange={handlePassword}/>
             </div>
           </div>
           <div className="flex-col gap-2">
