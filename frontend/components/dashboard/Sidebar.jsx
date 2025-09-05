6e9f2b48e83e6e0fc23bc313d3b4e8bad8bfaea6
Dhanush-Poduval <dhanushpoduval@gmail.com>
2025-09-05 17:37:51 +0530

    feat:fixed the issue of the sidebar content not shoiwng

--- frontend/components/dashboard/Sidebar.jsx
+++ frontend/components/dashboard/Sidebar.jsx
@@ -1,9 +1,9 @@
 'use client'
 import React, { useState } from 'react'
-import { Sidebar,SidebarContent, SidebarGroup, SidebarGroupLabel, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem, SidebarSeparator } from '../ui/sidebar'
+import { Sidebar,SidebarContent, SidebarGroup, SidebarGroupContent, SidebarGroupLabel, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem, SidebarSeparator } from '../ui/sidebar'
 import Link from 'next/link'
 import { Plus, Text } from 'lucide-react'
- const chats=[
+const chats=[
         {number:"1",title:"First Chat",icon:Text},
         {number:"1",title:"First Chat",icon:Text},
         {number:"1",title:"First Chat",icon:Text},
@@ -30,23 +30,29 @@ export default function AppSidebar() {
             <SidebarSeparator />
             <SidebarContent>
                 <SidebarGroup>
-                    <SidebarGroupLabel>{chat?chats.map(chits=>{
-                        <SidebarMenuItem key={chits.number}>
-                            <SidebarMenuButton>
-                            <Link href="/dashboard">
-                               <chits.icon />
-                               <span className='text-black'>{chits.title}</span>
-                            </Link>
+                    <SidebarGroupLabel>
+                        <SidebarGroupContent>
+                            <SidebarMenu>
+                                {chat?chats.map(chits=>(
+                                <SidebarMenuItem key={chits.number}>
+                                    <SidebarMenuButton>
+                                    <Link href="/dashboard">
+                                    <chits.icon />
+                                    <span>{chits.title}</span>
+                                    </Link>
 
-                            </SidebarMenuButton>
-                           
-                        </SidebarMenuItem>
-                        
+                                    </SidebarMenuButton>
+                                
+                                </SidebarMenuItem>
+                                
 
 
-                    }):(<SidebarMenuItem>
-                        <span className='font-bold text-center text-3xl'>No chats created yet!!</span>
-                    </SidebarMenuItem>)}</SidebarGroupLabel>
+                            )):(<SidebarMenuItem>
+                                <span className='font-bold text-center text-3xl'>No chats created yet!!</span>
+                            </SidebarMenuItem>)}
+                            </SidebarMenu>
+                        </SidebarGroupContent>
+                    </SidebarGroupLabel>
                 </SidebarGroup>
             </SidebarContent>
         </Sidebar>
